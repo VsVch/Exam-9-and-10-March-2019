c4afa043d01e68ad32711f895cf66bb7c4e800d4
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-27 18:05:31 +0300

    Add project files.

---
+++ 06. High Jump/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _06._High_Jump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double hihgWants = double.Parse(Console.ReadLine());
+
+            double highStarts = hihgWants - 30;
+            double highCurant = double.Parse(Console.ReadLine());
+            int br = 1;
+            
+
+            while (highCurant <= hihgWants)
+            {
+                
+                highCurant = double.Parse(Console.ReadLine());
+                br++;
+                int brFail = 0;
+                if (highCurant <= highStarts)
+                {
+                    
+                    brFail++;
+                    if (brFail == 3)
+                    {
+                        break;
+                    }
+
+
+                }
+                else if (highCurant >= hihgWants)
+                {
+                    brFail = 0;
+                    highStarts += 5;
+
+                }
+
+               
+
+
+            }
+
+
+            if (highCurant >= hihgWants)
+            {
+                Console.WriteLine($"Tihomir failed at {highCurant}cm after {br} jumps.");
+            }
+            else
+            {
+                Console.WriteLine($"Tihomir succeeded, he jumped over {hihgWants}cm after {br} jumps.");
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Exam  9 and 10 March 2019/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exam__9_and_10_March_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
